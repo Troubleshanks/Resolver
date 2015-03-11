495c33c1d3ab9c688ddee371b8b1b0da07bce9d6
troubleshanks <XT7BFY2l9O>
2015-03-10 18:00:45 -0700

    First commit of Resolver

---
+++ Resolver/App_Start/DatabaseConnectionContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MongoDB.Driver;
+using Resolver.Controllers;
+
+namespace Resolver.App_Start
+{
+    public class DatabaseConnectionContext
+    {
+        
+        public MongoDatabase database;
+
+
+        /// <summary>
+        /// DatabaseConnectionContext
+        /// Contains method to connect to mongodb
+        /// </summary>
+        public DatabaseConnectionContext()
+        {
+            try 
+            { 
+                //Implement client to connect to server
+                var client = new MongoClient(Resolver.Properties.Settings.Default.Server);
+                //Establish connection with server
+                MongoServer server = client.GetServer();
+                //Establish a conext to connect to database
+                database = server.GetDatabase(Resolver.Properties.Settings.Default.RootDatabase);
+                //Check if thread and platform collection exist, if not create
+
+
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+               
+            }
+        }
+
+        // TODO create method to return resolver thread objects from database
+        
+
+
+    }
+}
